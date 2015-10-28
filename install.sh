b776d47f3b44e43dce201a8046a2542f22ae976c
Alan Maciel <alan.maciel.salcedo@gmail.com>
2015-10-27 21:04:19 -0600

    Added gitconfig.local

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -s dotfiles-local/vimrc.local ~/.vimrc.local
 ln -s dotfiles-local/vimrc.bundles.local ~/.vimrc.bundles.local
 ln -s dotfiles-local/tmux.conf.local ~/.tmux.conf.local
 ln -s dotfiles-local/tmuxinator.zsh ~/.tmuxinator.zsh
+ln -s dotfiles-local/gitconfig.local ~/.gitconfig.local
 ln -s dotfiles-local/zshrc.local ~/.zshrc.local
 echo "Done! enjoy"

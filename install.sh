a4c7eb0c1ac1111ca1f748a79ec505965bcfeb16
Alan Maciel <alan.maciel.salcedo@gmail.com>
2015-10-27 20:30:32 -0600

    First commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Creating Symlinks in Home" 
+ln -s dotfiles-local/aliases.local ~/.aliases.local
+ln -s dotfiles-local/vimrc.local ~/.vimrc.local
+ln -s dotfiles-local/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s dotfiles-local/tmux.conf.local ~/.tmux.conf.local
+ln -s dotfiles-local/tmuxinator.zsh ~/.tmuxinator.zsh
+ln -s dotfiles-local/zshrc.local ~/.zshrc.local
+echo "Done! enjoy"

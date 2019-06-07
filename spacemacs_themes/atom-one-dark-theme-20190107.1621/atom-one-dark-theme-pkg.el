c13c03dc5876a6c0f7c3bbeaaa7f0bcac65dc606
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-06-07 10:01:47 -0500

    Added atom one dark custom theme

---
+++ spacemacs_themes/atom-one-dark-theme-20190107.1621/atom-one-dark-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "atom-one-dark-theme" "20190107.1621" "Atom One Dark color theme" 'nil :commit "f8441a3e495962df84bd649ca0366d1473ac741c" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")

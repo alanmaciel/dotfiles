bb3a5b1fcae716b644b7b9f31cf85f81b0674852
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-06-07 10:02:18 -0500

    Added Nord dark custom theme

---
+++ spacemacs_themes/nord-theme-20180913.1749/nord-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nord-theme" "20180913.1749" "An arctic, north-bluish clean and elegant theme" '((emacs "24")) :commit "c4e0b5548e2f3a149658617b33813cb115bdecfe" :authors '(("Arctic Ice Studio" . "development@arcticicestudio.com")) :maintainer '("Arctic Ice Studio" . "development@arcticicestudio.com") :url "https://github.com/arcticicestudio/nord-emacs")

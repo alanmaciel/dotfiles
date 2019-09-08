898631cd1d671dcff0b30cc10f668d2e44c2981c
Alan Maciel <alan.maciel.salcedo@gmail.com>
2019-09-08 12:06:25 -0500

    Added zshenv to install script

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+echo "Deleteing Symlinks in Home" 
+rm ~/.aliases.local
+rm ~/.vimrc.local
+rm ~/.vimrc.bundles.local
+rm ~/.tmux.conf.local
+rm ~/.tmuxinator.zsh
+rm ~/.gitconfig.local
+rm ~/.zshrc.local
+rm  ~/.gitignore
+rm ~/.spacemacs
 echo "Creating Symlinks in Home" 
 ln -s dotfiles-local/aliases.local ~/.aliases.local
 ln -s dotfiles-local/vimrc.local ~/.vimrc.local
@@ -6,6 +16,7 @@ ln -s dotfiles-local/tmux.conf.local ~/.tmux.conf.local
 ln -s dotfiles-local/tmuxinator.zsh ~/.tmuxinator.zsh
 ln -s dotfiles-local/gitconfig.local ~/.gitconfig.local
 ln -s dotfiles-local/zshrc.local ~/.zshrc.local
+ln -s dotfiles-local/zshenv.local ~/.zshenv.local
 ln -s dotfiles-local/gitignore.local ~/.gitignore
 ln -s dotfiles-local/spacemacs.local ~/.spacemacs
 echo "Done! enjoy"

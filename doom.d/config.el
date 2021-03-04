05746e376cd70cc0a14876f1e296ad7f73f86965
Alan Maciel <alan.maciel.salcedo@gmail.com>
2021-03-04 14:45:56 -0600

    Open emacs from the terminal

--- doom.d/config.el
+++ doom.d/config.el
@@ -5,6 +5,7 @@
 (setq fancy-splash-image "~/dotfiles-local/doom.d/lucky_256x256.png")
 (setq doom-font (font-spec :family "Operator Mono" :size 13))
 (when (eq system-type 'darwin)
+  (setq ns-pop-up-frames nil)
   (setq mac-right-option-modifier 'none))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -19,3 +20,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-to-list 'default-frame-alist '(ns-appearance . dark))

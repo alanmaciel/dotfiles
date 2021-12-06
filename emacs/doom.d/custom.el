64eddd600bdd02d4b558293b325d61b77b9b8639
Alan Maciel <alan.maciel.salcedo@gmail.com>
2021-12-06 11:15:52 -0600

    Updated doom emacs and improve some configuration and assets

--- emacs/doom.d/custom.el
+++ emacs/doom.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(feature-mode evil-escape)))
+ '(package-selected-packages '(vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

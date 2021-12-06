00ca8a51d16aa3a881cfbe45aff6ddc2a0b7c4f4
Alan Maciel <alan.maciel.salcedo@gmail.com>
2021-12-06 11:19:41 -0600

    Added script to install emacs additional tools

---
+++ emacs/install_doom_tools.sh
@@ -0,0 +1,17 @@
+brew install fd
+brew install gls
+brew install cmake
+brew install markdown-preview
+brew install isort
+brew install coreutils
+brew install nosetests
+brew install shellcheck
+brew install stylelint
+brew install node
+brew install wget
+brew install python3
+brew install cppcheck tinyxml eigen pcre asio tinyxml2 opencv
+brew install pipenv
+brew install pytest
+npm install -g marked
+npm install -g js-beautify
